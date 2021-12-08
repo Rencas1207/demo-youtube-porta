9f9a46fe0fd4e12c3fc97870b0ad178153e87148
Renzo Castilla <rencasdag.12@gmail.com>
2021-12-08 17:19:25 -0500

    About Finisth

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,134 +1,91 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { Design, Develope } from './AllSvgs';
+import styled, { keyframes, ThemeProvider } from 'styled-components';
+import { darkTheme } from './Themes';
 
 import { LogoComponent } from '../subComponents/LogoComponent';
 import { SocialIcons } from '../subComponents/SocialIcons';
 import { PowerButton } from '../subComponents/PowerButton';
 
-import { lightTheme } from './Themes';
 import { ParticleComponent } from '../subComponents/ParticleComponent';
+
+import astronaut from '../assets/Images/spaceman.png';
 
 const Box = styled.div`
   background: ${(props) => props.theme.body};
   width: 100vw;
   height: 100vh;
   position: relative;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+  overflow: hidden;
+`;
+
+const float = keyframes`
+  0% {transform: translateY(-10px)}
+  50% {transform: translateY(15px) translateX(15px)}
+  100% {transform: translateY(-10px)}
+`;
+
+const Spaceman = styled.div`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  width: 20vw;
+
+  animation: ${float} 4s ease infinite;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 const Main = styled.div`
   border: 2px solid ${(props) => props.theme.text};
   color: ${(props) => props.theme.text};
-  background: ${(props) => props.theme.body};
   padding: 2rem;
-  width: 30vw;
+  width: 50vw;
   height: 60vh;
   z-index: 3;
   line-height: 1.5;
-  cursor: pointer;
 
-  font-family: 'Ubuntu Mono', monospace;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  &:hover {
-    color: ${(props) => props.theme.body};
-    background: ${(props) => props.theme.text};
-  }
-`;
-
-const Title = styled.h2`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: calc(1em + 1vw);
+  font-size: calc(0.7rem + 1vw);
+  backdrop-filter: blur(4px);
 
-  ${Main}:hover & {
-    & > * {
-      fill: ${(props) => props.theme.body};
-    }
-  }
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
 
-  & > *:first-child {
-    margin-right: 1rem;
-  }
-`;
-
-const Description = styled.div`
-  color: ${(props) => props.theme.text};
-  font-size: calc(0.6em + 1vw);
-  padding: 0.5rem 0;
-
-  ${Main}:hover & {
-    color: ${(props) => props.theme.body};
-  }
-
-  strong {
-    margin-bottom: 1rem;
-    text-transform: uppercase;
-  }
-
-  ul,
-  p {
-    margin-left: 2rem;
-  }
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
 `;
 
 export const AboutPage = () => {
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <Box>
-        <LogoComponent theme="light" />
-        <SocialIcons theme="light" />
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
         <PowerButton />
 
-        <ParticleComponent theme="light" />
+        <ParticleComponent theme="dark" />
+
+        <Spaceman>
+          <img src={astronaut} alt="spaceman" />
+        </Spaceman>
 
         <Main>
-          <Title>
-            <Design width={40} height={40} /> Designer
-          </Title>
-          <Description>
-            I love to create design which speaks, Keep it clean, minimal and
-            simple.
-          </Description>
-          <Description>
-            <strong>I like to Design</strong>
-            <ul>
-              <li>Web Design</li>
-              <li>Mobile Apps</li>
-            </ul>
-          </Description>
-          <Description>
-            <strong>Tools</strong>
-            <ul>
-              <li>Figma</li>
-            </ul>
-          </Description>
-        </Main>
-        <Main>
-          <Title>
-            <Develope width={40} height={40} /> Frontend Developer
-          </Title>
-          <Description>
-            I value business or brand for which i'm creating, thus i enjoy
-            bringing new ideas to life.
-          </Description>
-          <Description>
-            <strong>Skills</strong>
-            <p>
-              Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase
-              etc.
-            </p>
-          </Description>
-          <Description>
-            <strong>Tools</strong>
-            <p>VScode, Github, Codepen etc.</p>
-          </Description>
+          I'm a front-end developer located in India. I love to create simple
+          yet beautiful websites with great user experience.
+          <br />
+          <br />
+          I'm interested in the whole frontend stack Like trying new things and
+          building great projects. I'm an independent freelancer and blogger.
+          <br />
+          <br />I love to write blogs and read books. I believe everything is an
+          Art when you put your consciousness in it. You can connect with me via
+          social links.
         </Main>
       </Box>
     </ThemeProvider>
